de84af6c8461c3b6dbaf20742a8e31657414f10c
Paul <paul@niobium.me>
2017-03-14 21:42:17 +0100

    selfbotting should work now

--- src/incoming.js
+++ src/incoming.js
@@ -2,16 +2,30 @@ import outgoing from "./outgoing"
 
 module.exports = (bp, discord, config) => {
   discord.on("messageCreate", msg => {
-    if(!bp.discord.isSelf(msg.author.id) || config.useSelf.get() == true) {
-      bp.middlewares.sendIncoming({
-        platform: "discord",
-        type: "message",
-        user: msg.author,
-        text: msg.content,
-        channel: msg.channel,
-        raw:  msg
-      })
+    if(config.useSelf.get()) {
+      if(bp.discord.isSelf(msg.author.id)) {
+        bp.middlewares.sendIncoming({
+          platform: "discord",
+          type: "message",
+          user: msg.author,
+          text: msg.content,
+          channel: msg.channel,
+          raw:  msg
+        })
+      }
+    }else{
+      if(!bp.discord.isSelf(msg.author.id)) {
+        bp.middlewares.sendIncoming({
+          platform: "discord",
+          type: "message",
+          user: msg.author,
+          text: msg.content,
+          channel: msg.channel,
+          raw:  msg
+        })
+      }
     }
+
   })
   discord.on("ready", () => {
     console.log("Discord ready")
